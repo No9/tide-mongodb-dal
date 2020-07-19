abeb9e58a6c64f8cbc6f127530ef828db56b7b1d
Anton Whalley <anton@venshare.com>
2020-07-19 02:13:59 +0100

    find by id

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use async_std::stream::StreamExt;
 use mongodb::{
-    bson::{Document},
+    bson::{doc, Document},
     Client,
 };
 use std::error;
@@ -21,6 +21,29 @@ pub async fn get(client : &Client, db_name: &str, collection_name : &str) -> Res
     }
     Ok(docs)
 }
+
+pub async fn get_by_id(client : &Client, db_name: &str, collection_name : &str, objid: &str) -> Result<Document, Box<dyn error::Error>> {
+    
+    let db = client.database(db_name);
+    let coll = db.collection(collection_name);
+    let filter = doc! { "_id": objid };
+    let mut cursor = coll.find(filter, None).await?;
+
+    // let mut cursor = coll.find(None, None).await?;
+    let mut doc = Document::new();
+    while let Some(result) = cursor.next().await {
+        match result {
+            Ok(document) => {
+                doc = document;
+            }
+            Err(e) => println!("Err {}", e),
+        }
+    }
+    Ok(doc)
+}
+
+
+
 
 #[cfg(test)]
 mod tests {
